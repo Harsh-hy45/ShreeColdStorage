9d881050152d12bc4587838faec0f6a881f2bb5d
Harsh-hy45 <harshchoudhary271001@gmail.com>
2025-01-13 13:10:37 +0530

    HARSH - Landing Page and Advantages.

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -8,7 +8,7 @@ import {
 } from "json/landingPageData";
 import Header from "parts/Header";
 import Hero from "parts/Hero";
-import Service from "parts/Service";
+import Service from "parts/ServicesPage/Service";
 import Portfolio from "parts/Portfolio";
 import Advantage from "parts/Advantage";
 import Testimonial from "parts/Testimonial";
