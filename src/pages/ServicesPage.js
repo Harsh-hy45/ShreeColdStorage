a1a294a8188b37a062fc4eb0464c48c666a3c970
Harsh-hy45 <harshchoudhary271001@gmail.com>
2025-03-21 19:11:16 +0530

    HARSH: service page orientation

--- src/pages/ServicesPage.js
+++ src/pages/ServicesPage.js
@@ -3,14 +3,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-filename-extension */
 import React, { Component } from "react";
-
+import { Services, Portfolios } from "json/landingPageData";
 import Header from "parts/Header";
 import ServicePortfolio from "parts/ServicesPage/ServicePortfolio";
 import Discuss from "parts/DiscussProjectPage/Discuss";
+import Service from "parts/ServicesPage/Service";
 import Footer from "parts/Footer";
 import AllPortfolio from "parts/LandingPage/AllPortfolio";
-
-import { Portfolios } from "json/landingPageData";
 
 export default class ServicesPage extends Component {
   componentDidMount() {
@@ -22,7 +21,8 @@ export default class ServicesPage extends Component {
       <>
         <Header />
         <ServicePortfolio />
-        <AllPortfolio data={Portfolios} />
+        <Service data={Services} />
+        {/* <AllPortfolio data={Portfolios} /> */}
         <Discuss />
         <Footer />
       </>
--- src/parts/ServicesPage/ServicePortfolio.js
+++ src/parts/ServicesPage/ServicePortfolio.js
@@ -78,7 +78,6 @@ export default function ServicePortfolio() {
           </p>
         </Fade>
       </div>
-
       {/* Image Section */}
       <div className="flex justify-center">
         <Fade direction="up" triggerOnce>
@@ -89,7 +88,6 @@ export default function ServicePortfolio() {
           />
         </Fade>
       </div>
-
       {/* Services List */}
       <div className="lg:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
         {services.map((service, index) => (
@@ -116,59 +114,3 @@ export default function ServicePortfolio() {
     </section>
   );
 }
-
-<div className="bg-gray-50">
-  <div className="container mx-auto pt-20 pb-28">
-    <Fade direction="right" triggerOnce>
-      <h1 className="text-5xl text-theme-blue text-center font-bold mb-8">
-        Cold Storage Solutions for Your Growing Needs
-      </h1>
-    </Fade>
-    <Fade direction="left" triggerOnce>
-      <p
-        className={`${styles2.mainDescription} font-light text-lg text-gray-400 text-center mx-auto`}
-      >
-        At Shree Cold Storage, we are committed to safeguarding your perishable
-        goods with state-of-the-art technology and precise care. Explore our
-        services tailored to meet the diverse needs of your business.
-      </p>
-    </Fade>
-
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 px-10">
-      {data.map((item, index) => (
-        <Fade
-          direction={item.animation}
-          delay={500 * index}
-          key={index}
-          triggerOnce
-        >
-          <div
-            className={`${styles2.serviceCard} group`}
-            onClick={() => handleToggle(index)}
-          >
-            <div
-              className={`${styles2.imageContainer} relative w-full h-80 overflow-hidden`}
-            >
-              <img
-                src={item.imageUrl}
-                alt={item.title}
-                className={`${styles2.serviceImage} absolute inset-0 w-full h-full object-cover transition-transform duration-500 group-hover:scale-110`}
-              />
-            </div>
-            <h2 className="text-theme-blue text-center text-xl py-7 bg-white rounded-b-2xl">
-              {item.title}
-            </h2>
-
-            {activeIndex === index && (
-              <div className={`${styles2.serviceDescriptionTab} bg-white`}>
-                <p className="text-center text-gray-700 py-3">
-                  {item.description}
-                </p>
-              </div>
-            )}
-          </div>
-        </Fade>
-      ))}
-    </div>
-  </div>
-</div>;
